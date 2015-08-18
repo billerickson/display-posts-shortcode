123f509e51c8a93327af6859bfbef6edb275df05
Bill Erickson <bill.erickson@gmail.com>
2015-08-18 13:34:28 -0500

    add empty $category_display_text

--- display-posts-shortcode.php
+++ display-posts-shortcode.php
@@ -415,6 +415,7 @@ function be_display_posts_shortcode( $atts ) {
 		}
 		
 		// Display categories the post is in
+		$category_display_text = '';
 		if( $category_display && is_object_in_taxonomy( get_post_type(), $category_display ) ) {
 			$terms = get_the_terms( get_the_ID(), $category_display );
 			$term_output = array();
