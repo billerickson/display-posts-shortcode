290dab750749e395903f8f69cf28c7c9b2d7c5f0
Henry Jobst <henry@jobst-berlin.de>
2021-02-04 08:11:34 +0100

    add text domain

--- display-posts-shortcode.php
+++ display-posts-shortcode.php
@@ -6,6 +6,7 @@
  * Version: 3.0.2
  * Author: Bill Erickson
  * Author URI: https://www.billerickson.net
+ * Text Domain: display-posts
  *
  * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
  * General Public License version 2, as published by the Free Software Foundation.  You may NOT assume
